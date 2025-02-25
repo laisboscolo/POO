5857371cdd41372ea9535ca95e1980e68ed502df
Laís Boscolo <costaboscololais2008@gmail.com>
2025-02-25 08:08:02 -0300

    Initial commit

---
+++ objeto.js
@@ -0,0 +1,11 @@
+// criando um objeto aluno
+const aluno = {
+    nome: "Lais",
+    idade: 16,
+
+    estudar: () => console.log
+    (`${aluno.nome} está estudando.`),
+};
+
+console.log(aluno.nome);
+aluno.estudar();
